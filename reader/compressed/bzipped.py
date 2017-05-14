66d86193990836a57e35e0cfcf4b483187ed9b48
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-14 19:40:27 +0300

    gzipped fix

--- reader/compressed/bzipped.py
+++ reader/compressed/bzipped.py
@@ -1,9 +1,9 @@
-import gzip
+import bz2
 import sys
 
-opener = gzip.open
+opener = bz2.open
 
 if __name__ == '__main__':
-    f = gzip.open(sys.argv[1], mode='wt')
+    f = bz2.open(sys.argv[1], mode='rt')
     f.write(' '.join(sys.argv[2:]))
     f.close()
